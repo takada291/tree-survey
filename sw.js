6cc120dcdac5021d078754d0a27683e8c54fdf38
takada291 <tkp291@gmail.com>
2026-01-02 13:54:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'tree-survey-v1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  'sw.js',
+  'icon-192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+});
