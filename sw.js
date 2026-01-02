81c5206f9c3b5e1f8598deab76aea10c94ab999b
takada291 <tkp291@gmail.com>
2026-01-02 14:43:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tree-survey-v2';
+const CACHE_NAME = 'tree-survey-final-v1'; // 名前を一新
 const urlsToCache = [
   './',
   'index.html',
@@ -8,11 +8,23 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // 一つずつキャッシュして、失敗したファイルがわかるようにする
+      return Promise.all(
+        urlsToCache.map((url) => {
+          return cache.add(url).catch(err => console.error('キャッシュ失敗:', url));
+        })
+      );
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
-
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
